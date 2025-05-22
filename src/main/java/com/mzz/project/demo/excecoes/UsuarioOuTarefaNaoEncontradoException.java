df0b4e59005e4d1195a74c42c5f1f42f158e93b2
Mzdudul <mzzdev.e@gmail.com>
2025-05-22 13:37:31 -0300

    removendo arquivo

--- src/main/java/com/mzz/project/demo/excecoes/UsuarioOuTarefaNaoEncontradoException.java
+++
@@ -1,7 +0,0 @@
-package com.mzz.project.demo.excecoes;
-
-public class UsuarioOuTarefaNaoEncontradoException extends RuntimeException {
-    public UsuarioOuTarefaNaoEncontradoException(String mensagem){
-        super(mensagem);
-    }
-}
